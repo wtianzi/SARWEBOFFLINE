65191911a94a3ea3c357164e635432044a884d49
Tianzi Wang <wtianzi@gmail.com>
2022-07-10 22:31:59 -0400

    download to server

--- app3/py/blobdownload.py
+++ app3/py/blobdownload.py
@@ -12,16 +12,35 @@ def downloadFromURL_org(urlin,sourcename,tileindex0,tileindex1,tileindex2):
         ff.write(file.content)
     return True
 
-def downloadFromURL(urimg,sourcename,tilename):
+def downloadFromURL_org(urimg,sourcename,tilename):
     rootfolder="./static/maptile/"
     x=tilename.split('/')
     imgfolder=rootfolder+sourcename+"/"+x[0]+"/"+x[1]+"/"
     if not os.path.isdir(imgfolder):
         os.makedirs(imgfolder)
-    #print(urimg)
+    print(urimg)
     filename=rootfolder+sourcename+"/"+tilename+".png"
-    #print(os.path.abspath(filename))
+    print(os.path.abspath(filename))
     file=requests.get(urimg)
+    with open(filename,'wb') as ff:
+        ff.write(file.content)
+    return True
+
+def downloadFromURL(url,sourcename,tilename):
+    rootfolder="./static/maptile/"
+    x=tilename.split('/')
+    imgfolder=rootfolder+sourcename+"/"+x[0]+"/"+x[1]+"/"
+    if not os.path.isdir(imgfolder):
+        os.makedirs(imgfolder)
+    print(url)
+    filename=rootfolder+sourcename+"/"+tilename+".png"
+    print(os.path.abspath(filename))
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    file=requests.get(url,headers=headers)
+    print(file)
+    if os.path.exists(filename):
+        os.remove(filename)
     with open(filename,'wb') as ff:
         ff.write(file.content)
     return True
